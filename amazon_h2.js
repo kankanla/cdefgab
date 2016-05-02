05b27e95f677bf486513ae1f257c862448c2f834
kankanla <koyi_s@hotmail.com>
2016-05-02 12:27:03 +0900

    Added files via upload

--- amazon_h2.js
+++ amazon_h2.js
@@ -8,7 +8,8 @@ x = function (){
 		// var ama_url ='http://www.amazon.co.jp/s/?_encoding=UTF8&camp=1207&creative=8415&linkCode=shr&tag=ak8-22&_mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&url=search-alias%3Daps&field-keywords=';
 
 		if(x[i].getAttribute('data-attribute')){
-			ama_url = ama_url + encodeURIComponent(x[i].getAttribute('data-attribute'));
+			// ama_url = ama_url + encodeURIComponent(x[i].getAttribute('data-attribute'));
+			ama_url = ama_url + x[i].getAttribute('data-attribute');
 			text = text +'<a class="xxx" href="' + ama_url + '" thile="' + x[i].getAttribute('data-attribute') + '" target="_blank" rel="follow">' + x[i].getAttribute('data-attribute') + '</a><br>';
 		}
 	}
